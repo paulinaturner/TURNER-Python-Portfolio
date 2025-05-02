306499b13767cb7ac628388942cc25c4f85da5b2
Paulina Turner <pturnerc@nd.edu>
2025-05-01 22:46:41 -0400

    Update FinalStreamlitApp.py

--- StreamlitAppFinal/FinalStreamlitApp.py
+++ StreamlitAppFinal/FinalStreamlitApp.py
@@ -1,6 +1,8 @@
 import pandas as pd #Importing pandas library and setting pd as a referral to it
 import matplotlib.pyplot as plt  # Importing the necessary library for plotting
-import streamlit as st
-import spacy
-from spacy.pipeline import EntityRuler
-from spacy import displacy
+import streamlit as st #Importing streamlit as st
+import spacy #Importing spcy
+from spacy.pipeline import EntityRuler #From spacy , import the Entity Ruler
+from spacy import displacy #From the spacy world, import displacy
+
+
